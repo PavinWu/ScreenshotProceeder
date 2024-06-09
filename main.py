d6c0d64bd22dc6088c39fee02ebc1dec2b4cc7eb
Pavin Wu <pavin.wu@gmail.com>
2024-06-09 14:52:21 +1000

    First commit - outline what to do

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# arguments: number of key presses, key press, application, wait time for each key
+
+# capture screenshot
+# show UI to user
+# read four corners from user
+    # check that it makes up four corners
+# create folder
+# repeat
+    # select appliation, capture screenshot (and crop) and send key
+# (optional) combine into PDF?
