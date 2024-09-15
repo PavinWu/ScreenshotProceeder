6153b7a5d431d9f14be15889e7d0902f38821964
Pavin Wu <pavin.wu@gmail.com>
2024-09-15 15:28:34 +1000

    Finally showing rubberband, but screenshot size is still wrong

--- src/areaSelector.py
+++ src/areaSelector.py
@@ -11,13 +11,14 @@ class AreaSelector(QtWidgets.QWidget):
         # Note that begin coords may be less or greater than end.
         self.beginCoords = QRect()
         self.endCoords = QRect()
-        self.areaSelectedSem = threading.Semaphore()
         self.screenShooter = ScreenShooter()
 
-        # Don't pass parent to have rubber band be top level.
+        self.screenshotLabel = QtWidgets.QLabel(self)
+        self.layout = QtWidgets.QStackedLayout(self)
+        self.layout.addWidget(self.screenshotLabel)
 
-    # TODO mouse event not registered for non-main window???
-    # Apparently, these mouse events require main window by default ...
+        self.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+
     def mousePressEvent(self, event):
         self.beginCoords = event.pos()
         # TODO normalize coords here?
@@ -32,44 +33,26 @@ class AreaSelector(QtWidgets.QWidget):
     def mouseReleaseEvent(self, event):
         self.endCoords = event.pos()
         self.rubberBand.hide()
-        # self.areaSelectedSem.release()
+        self.__hidePixmapFullscreen__()
 
     def getCoords(self):
         pixmap = self.screenShooter.getWholeScreen()
-        self.setEnabled(True)
-        screenshotLabel = self.__showPixmapFullScreen__(pixmap)
-
-
-        # Looks like we need to create an overlay first.
-
-        # self.areaSelectedSem.acquire()       
-        # self.setEnabled(False)
-        # self.__hidePixmapFullscreen__(screenshotLabel)
-
+        self.screenshotLabel = self.__showPixmapFullScreen__(pixmap)
         return self.beginCoords, self.endCoords
 
     def __showPixmapFullScreen__(self, pixmap):
-
-        screenshotLabel = QtWidgets.QLabel(self)
-
-        print("Label size: {} x {}", screenshotLabel.width(), screenshotLabel.height())
+        # TODO remove
+        print("Label size: {} x {}", self.screenshotLabel.width(), self.screenshotLabel.height())
         screen = QtGui.QGuiApplication.primaryScreen()
-        # TODO incorrect screen size when using external screen
+        # TODO remove. incorrect screen size?
         print("Screen size: {} x {}", screen.geometry().width(), screen.geometry().height())
-        
-        screenshotLabel.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
-        screenshotLabel.setAlignment(QtCore.Qt.AlignCenter)
-        # TODO set qlabel size to screen size.
-        # screenshotLabel.setPixmap(pixmap.scaled(
-        #         screenshotLabel.size(),
-        #         QtCore.Qt.KeepAspectRatio,
-        #         QtCore.Qt.SmoothTransformation))
-        screenshotLabel.setPixmap(pixmap)
-        screenshotLabel.showFullScreen()
 
-        return screenshotLabel
+        self.screenshotLabel.setPixmap(pixmap.scaled(
+                self.screenshotLabel.size(),
+                QtCore.Qt.KeepAspectRatio,
+                QtCore.Qt.SmoothTransformation))
+        self.show()
 
-    def __hidePixmapFullscreen__(self, screenshotLabel):
-        screenshotLabel.setWindowFlags(self.windowFlags() & ~QtCore.Qt.FramelessWindowHint & ~QtCore.Qt.WindowStaysOnTopHint)
-        screenshotLabel.hide()
+    def __hidePixmapFullscreen__(self):
+        self.hide()
 
